815131d09c3e2dcbae2b8ccc0e761be4786c36bf
LuchoCassani <lucho_92@hotmail.com>
2022-06-18 19:30:43 -0300

    funciones de validacion signUP

--- app/src/main/java/com/melvin/ongandroid/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/SignUpViewModel.kt
@@ -1,30 +1,25 @@
 package com.melvin.ongandroid.viewmodel
 
 
-import android.view.View
-import androidx.core.util.PatternsCompat
 
+import androidx.core.util.PatternsCompat
 import androidx.lifecycle.ViewModel
 import java.util.regex.Pattern
 
 class SignUpViewModel : ViewModel() {
 
+    fun validateFields(username: String, email: String, password: String, confirmPassword: String) {
 
-    fun validateFields(username:String, email:String, password:String, confirmPassword:String): Boolean{
-        return if (email.isEmpty() && password.isEmpty()) {
-            email.error = "Debes ingresar email"
-            false
-        } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(email.toString()).matches()) {
-            email.error = "Ingresar un email valido"
-            false
-        } else {
-            email.error = null
-            true
-        }
+        val fieldsEmpty: Boolean =
+            username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()
+        val emailFormat: Boolean = validateEmail(email)
+        val passwordsFormat: Boolean =
+            validatePassword(password) && isValidPasswordFormat(confirmPassword)
+
+
     }
 
-     fun validateEmail(email: View): Boolean {
-       // val email = binding.tiUserEmail.editText?.text.toString()
+      fun validateEmail(email: String): Boolean {
         return if (email.isEmpty()) {
             email.error = "Debes ingresar email"
             false
@@ -38,29 +33,18 @@ class SignUpViewModel : ViewModel() {
 
     }
 
-     fun validatePassword(): Boolean {
-        val password = binding.tiUserPassword.editText?.text.toString()
-        val confirmPassword = binding.tiUserPasswordConfirm.editText?.text.toString()
-        val passwordRegex = Pattern.compile(
-            "^" +
-                    "(?=.*[0-9])" +         //at least 1 digit
-                    "(?=.*[a-z])" +        //at least 1 lower case letter
-                    "(?=.*[A-Z])" +        //at least 1 upper case letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{4,}" +               //at least 4 characters
-                    "$",
-        )
-        return if (password.isEmpty()) {
-            binding.tiUserPassword.error = "Debes ingresar un password"
-            false
-        } else if (!passwordRegex.matcher(password).matches()) {
-            binding.tiUserPassword.error = "Password es muy debil"
-            false
-        } else (password) {
-            binding.tiUserPassword.error = null
-            true
-        }
+     fun validatePassword(password: String): Boolean {
+         val passwordRegex = Pattern.compile(
+             "^" +
+                     "(?=.*[0-9])" +         //at least 1 digit
+                     "(?=.*[a-z])" +        //at least 1 lower case letter
+                     "(?=.*[A-Z])" +        //at least 1 upper case letter
+                     "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                     "(?=\\S+$)" +           //no white spaces
+                     ".{4,}" +               //at least 4 characters
+                     "$",
+         )
+         return Pattern.matches(passwordRegex.toString(), password)
     }
 
      fun validate(){
@@ -68,7 +52,7 @@ class SignUpViewModel : ViewModel() {
         if(false in result){
             return
         }
-        binding.btnSignUp.enable = true
+        binding.btnSignUp.isEnabled = true
 
     }
 
