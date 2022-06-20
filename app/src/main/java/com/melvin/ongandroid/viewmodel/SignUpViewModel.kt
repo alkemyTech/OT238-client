85c7d1763f69a0e4b50c742d0bbf21fc4335d9f9
LuchoCassani <lucho_92@hotmail.com>
2022-06-19 23:21:32 -0300

    implementada la logica de los use cases en el viewModel

--- app/src/main/java/com/melvin/ongandroid/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/SignUpViewModel.kt
@@ -2,52 +2,87 @@ package com.melvin.ongandroid.viewmodel
 
 
 
-import androidx.core.util.PatternsCompat
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.google.android.material.textfield.TextInputEditText
-import java.util.regex.Pattern
+import androidx.lifecycle.viewModelScope
+import com.melvin.ongandroid.businesslogic.use_cases.ValidateConfirmPassword
+import com.melvin.ongandroid.businesslogic.use_cases.ValidateEmail
+import com.melvin.ongandroid.businesslogic.use_cases.ValidatePassword
+import com.melvin.ongandroid.businesslogic.use_cases.ValidateUserName
+import com.melvin.ongandroid.view.RegistrationFormEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
-class SignUpViewModel : ViewModel() {
-
-    /*fun validateFields(username: String, email: String, password: String, confirmPassword: String) {
-
-        val fieldsEmpty: Boolean =
-            username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()
-        val emailFormat: Boolean = validateEmail(email)
-        val passwordsFormat: Boolean =
-            validatePassword(password) && isValidPasswordFormat(confirmPassword)
+import javax.inject.Inject
 
 
-    }*/
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val validateEmail: ValidateEmail,
+    private val validatePassword: ValidatePassword,
+    private val validateUserName: ValidateUserName,
+    private val validateConfirmPassword: ValidateConfirmPassword
+) :ViewModel() {
 
-      fun validateEmail(email: TextInputEditText): Boolean {
-        return if (email.toString().isEmpty()) {
-            email.error = "Debes ingresar email"
-            false
-        } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(email.toString()).matches()) {
-            email.error = "Ingresar un email valido"
-            false
-        } else {
-            email.error = null
-            true
+    var state by mutableStateOf(RegistrationFormState())
+
+    private val validationEventChannel = Channel<ValidationEvent>()
+    val validationEvents = validationEventChannel.receiveAsFlow()
+
+    fun onEvent(event: RegistrationFormEvent){
+        when(event){
+            is RegistrationFormEvent.EmailChanged ->{
+                state = state.copy(email = event.email)
+            }
+            is RegistrationFormEvent.PasswordChanged ->{
+                state = state.copy(password = event.password)
+            }
+            is RegistrationFormEvent.ConfirmPasswordChanged ->{
+                state = state.copy(confirmPassword = event.confirmPassword)
+            }
+            is RegistrationFormEvent.UserNameChanged ->{
+                state = state.copy(userName = event.userName)
+            }
+            is RegistrationFormEvent.Submit ->{
+                submitData()
+
+            }
         }
-
     }
 
-     fun validatePassword(password: TextInputEditText): Boolean {
-         val passwordRegex = Pattern.compile(
-             "^" +
-                     "(?=.*[0-9])" +         //at least 1 digit
-                     "(?=.*[a-z])" +        //at least 1 lower case letter
-                     "(?=.*[A-Z])" +        //at least 1 upper case letter
-                     "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                     "(?=\\S+$)" +           //no white spaces
-                     ".{4,}" +               //at least 4 characters
-                     "$",
-         )
-         return Pattern.matches(passwordRegex.toString(), password.toString())
-    }
+    private fun submitData() {
+        val emailResult = validateEmail.executeEmailValidation(state.email)
+        val passwordResult = validatePassword.executePasswordValidation(state.password)
+        val userNameResult = validateUserName.executeUserNameValidation(state.userName)
+        val confirmPasswordResult = validateConfirmPassword.executeConfirmPasswordValidation(state.password, state.confirmPassword)
 
+        val hasError = listOf(
+            emailResult,
+            passwordResult,
+            userNameResult,
+            confirmPasswordResult
+        ).any { !it.successful }
+
+        if(hasError){
+            state = state.copy(
+                emailError = emailResult.errorMessage,
+                passwordError = passwordResult.errorMessage,
+                userNameError = userNameResult.errorMessage,
+                confirmPasswordError = confirmPasswordResult.errorMessage
+            )
+            return
+        }
+        viewModelScope.launch {
+            validationEventChannel.send(ValidationEvent.Success)
+        }
+    }
+    sealed class ValidationEvent{
+        object Success: ValidationEvent()
+    }
 
 
 }
