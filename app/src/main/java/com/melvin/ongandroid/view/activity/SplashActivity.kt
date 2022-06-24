5fa9f9ddc6c5e5846a70e4a70d80c6a2fcd4a4fc
pableArg <pablo.jc95@hotmail.com>
2022-06-24 12:51:19 -0300

    Desarrollo del splash. Falta resolver la imagen

---
+++ app/src/main/java/com/melvin/ongandroid/view/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.melvin.ongandroid.view.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
