c7fe24b82596270afb1a02aad1eacf721f23c4d8
Fran <franpepper94@gmail.com>
2022-06-25 16:08:07 -0300

    Added timer to wait 5 seconds after start

--- app/src/main/java/com/melvin/ongandroid/view/activity/SplashActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/activity/SplashActivity.kt
@@ -3,20 +3,36 @@ package com.melvin.ongandroid.view.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.melvin.ongandroid.data.AppData
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
-class SplashActivity() : AppCompatActivity(){
+class SplashActivity : AppCompatActivity(){
     @Inject lateinit var appData : AppData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkAuth()
+        timer()
     }
 
-    fun checkAuth(){
+    private fun timer(){
+        val timer = Executors.newSingleThreadScheduledExecutor()
+        timer.schedule(
+            {
+                runOnUiThread {
+                    checkAuth()
+                    Toast.makeText(this, "Timer has finished", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            },5,TimeUnit.SECONDS)
+    }
+
+    private fun checkAuth(){
         val token = appData.getKey().toString()
         if(token != ""){
             startActivity(Intent(this, HomeActivity::class.java))
