e182b334ab90fe04d4bd1fe024b4e08d931263ec
LuchoCassani <lucho_92@hotmail.com>
2022-06-25 18:22:12 -0300

    implemetado el RV de actividades del endpoint Slides

--- app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.melvin.ongandroid.databinding.FragmentHomeBinding
-import com.melvin.ongandroid.databinding.SlidesActivitiesListBinding
 import com.melvin.ongandroid.view.adapters.SlideAdapter
-import com.melvin.ongandroid.view.viewHolders.SlidesViewHolder
 import com.melvin.ongandroid.viewmodel.HomeViewModel
-import com.melvin.ongandroid.viewmodel.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -43,10 +37,7 @@ class HomeFragment : Fragment() {
         viewModel.slideList.observe(viewLifecycleOwner) {
             showActivities(homeViewModel, binding)
         }
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
 
     }
