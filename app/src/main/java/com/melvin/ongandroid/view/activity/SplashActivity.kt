69cf4f2681f812ac6eebd3cd1038f3154c20fa7e
Fran <franpepper94@gmail.com>
2022-06-24 22:30:51 -0300

    Added shared preferences

--- app/src/main/java/com/melvin/ongandroid/view/activity/SplashActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/activity/SplashActivity.kt
@@ -3,8 +3,14 @@ package com.melvin.ongandroid.view.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.melvin.ongandroid.data.AppData
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class SplashActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class SplashActivity() : AppCompatActivity(){
+    @Inject lateinit var appData : AppData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startActivity(Intent(this, MainActivity::class.java))
