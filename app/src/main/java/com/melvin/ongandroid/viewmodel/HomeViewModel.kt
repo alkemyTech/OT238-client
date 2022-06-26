9ef8b43bf7e165bf84567dd6b78b7919fc4ead0c
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-06-26 01:58:09 -0300

    Created viewPager layout, fixed HomeViewModel

--- app/src/main/java/com/melvin/ongandroid/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/HomeViewModel.kt
@@ -12,34 +12,25 @@ import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
-    class HomeViewModel @Inject constructor(
-        private val apiSlides: OngApi
-    ) : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val apiSlides: OngApi): ViewModel() {
 
-        init {
-            getSlides()
-        }
+    init {
+        getSlides()
+    }
 
-        private val _text = MutableLiveData<String>().apply {
-            value = "This is home Fragment"
-        }
-        val text: LiveData<String> = _text
+    private val _slideList = MutableLiveData<SlidesResponse>()
+    val slideList: LiveData<SlidesResponse> = _slideList
 
-        private val _slideList = MutableLiveData<SlidesResponse>()
-        val slideList: LiveData<SlidesResponse>
-            get() = _slideList
-
-        private fun getSlides() {
-            var slideActivities: SlidesResponse
-            viewModelScope.launch {
-                try {
-                    slideActivities = apiSlides.getSlides()
-                    _slideList.value = slideActivities
-                } catch (e: Exception) {
-                    slideActivities = SlidesResponse(false, emptyList())
-                    _slideList.value = slideActivities
-                }
+    private fun getSlides() {
+        var slideActivities: SlidesResponse
+        viewModelScope.launch {
+            try {
+                slideActivities = apiSlides.getSlides()
+                _slideList.value = slideActivities
+            } catch (e: Exception) {
+                slideActivities = SlidesResponse(false, emptyList())
+                _slideList.value = slideActivities
             }
         }
     }
