f737b72afb53655182789e3342ad744bfcfa8f71
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-06-28 14:33:48 -0300

    Deleted unused recyclerview

--- app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.FragmentHomeBinding
 import com.melvin.ongandroid.model.entities.slides.Slide
 import com.melvin.ongandroid.view.adapters.HomeViewPagerAdapter
-import com.melvin.ongandroid.view.adapters.SlideAdapter
-import com.melvin.ongandroid.viewmodel.ApiStatus
 import com.melvin.ongandroid.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,10 +19,6 @@ class HomeFragment : Fragment() {
     private val viewModel: HomeViewModel by viewModels()
     private var _binding: FragmentHomeBinding? = null
     private lateinit var adapter: HomeViewPagerAdapter
-    private lateinit var sliderAdapter: SlideAdapter
-    private val slideList = mutableListOf<Slide>()
-
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -40,20 +31,14 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentHomeBinding.bind(view)
-        initActivitiesRv()
         setupSlide()
 
     }
 
-    private fun initActivitiesRv() {
-        binding.rvActivitiesSlides.layoutManager =
-            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-    }
     private fun setupSlide() {
         viewModel.getSlides()
         setupObserver()
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -65,7 +50,6 @@ class HomeFragment : Fragment() {
         binding.pager.adapter = adapter
         adapter.submitList(data)
     }
-
 
     private fun setupObserver() {
         viewModel.observerSlideList().observe(viewLifecycleOwner) {
@@ -85,8 +69,6 @@ class HomeFragment : Fragment() {
             }
             .show()
     }
-
-
 
 }
 
--- app/src/main/java/com/melvin/ongandroid/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/HomeViewModel.kt
@@ -1,24 +1,18 @@
 package com.melvin.ongandroid.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.android.material.slider.Slider
 import com.melvin.ongandroid.data.ApiClient
-import com.melvin.ongandroid.data.OngApi
 import com.melvin.ongandroid.model.entities.slides.Slide
-import com.melvin.ongandroid.model.entities.slides.SlidesResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val repository: ApiClient
 ) : ViewModel() {
-
 
     private val _status = MutableLiveData<ApiStatus>()
     private val _slideList = MutableLiveData<List<Slide>>()
@@ -38,10 +32,7 @@ class HomeViewModel @Inject constructor(
                 _slideList.value = emptyList()
                 _status.value = ApiStatus.FAILURE
             }
-
         }
-
     }
-
 
 }
