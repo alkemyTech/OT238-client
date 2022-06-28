86853fec230a11f2363f230748684acd673f5017
xavier eloy rosales <rosales.xavier.eloy@gmail.com>
2022-06-28 12:34:25 -0300

    dialog error News(OT238-38)

--- app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.melvin.ongandroid.view.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -32,8 +34,6 @@ class HomeFragment : Fragment() {
   }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentHomeBinding.bind(view)
-
-
     }
     private fun loadNewsPager(data: List<News>){
         newsAdapter = NewsAdapter()
@@ -46,14 +46,25 @@ class HomeFragment : Fragment() {
             if (viewModel.observeNewsList() != null){
 
             }else{
-
+                dialogNews()
             }
-
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun dialogNews(){
+        val dialog = AlertDialog.Builder(context)
+            .setTitle(R.string.dialog_news_error_title)
+            .setMessage(R.string.dialog_news_error_message)
+            .setPositiveButton(R.string.dialog_news_error_positive_btn) { view, _ ->
+                setUpObserver()
+            }
+            .setCancelable(false)
+            .create()
+        dialog.show()
     }
 }
