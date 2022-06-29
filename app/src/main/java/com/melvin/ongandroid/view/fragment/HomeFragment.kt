3aa4b549c37fddf98159a86afa52fbeb66913bc4
LuchoCassani <lucho_92@hotmail.com>
2022-06-28 21:41:33 -0300

    created getnews func in home viewmodel and instantiated in home fragment

--- app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/HomeFragment.kt
@@ -5,11 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.FragmentHomeBinding
 import com.melvin.ongandroid.model.entities.News
@@ -35,7 +32,12 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentHomeBinding.bind(view)
+        setUpNews()
 
+    }
+
+    private fun setUpNews() {
+        viewModel.getNews()
         setUpObserver()
     }
 
--- app/src/main/java/com/melvin/ongandroid/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/HomeViewModel.kt
@@ -2,18 +2,35 @@ package com.melvin.ongandroid.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.melvin.ongandroid.data.ApiClient
 import com.melvin.ongandroid.model.entities.News
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
+    private val repository : ApiClient
 ): ViewModel() {
 
+    private val _status = MutableLiveData<ApiStatus>()
     private val _newsList = MutableLiveData<List<News>>()
+
     fun observeNewsList (): MutableLiveData<List<News>>{
         return this._newsList
     }
 
+    fun getNews(){
+        viewModelScope.launch {
+            val response = repository.getNews()
+            if (response.success){
+                _newsList.value = response.data
+                _status.value = ApiStatus.SUCCESS
+            }else{
+                _newsList.value = emptyList()
+                _status.value = ApiStatus.FAILURE
+            }
+        }
+    }
 }
