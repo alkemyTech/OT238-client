7d19a49c29cb0c92d7318958d9afdf8eb9ce608b
Fran <franpepper94@gmail.com>
2022-06-30 16:56:04 -0300

    Removed dummy data

--- app/src/main/java/com/melvin/ongandroid/view/fragment/TestimonialsFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/TestimonialsFragment.kt
@@ -21,42 +21,6 @@ class TestimonialsFragment : Fragment() {
     private var _binding: FragmentTestimonialsBinding? = null
     private val binding get() = _binding!!
 
-    //Dummy Data for test purposes
-    var testimonialsDummyData = listOf(
-        TestimonialsResponse(
-            1,
-            "John Doe",
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "",
-            "",
-            "",),
-        TestimonialsResponse(
-            2,
-            "Jane Doe",
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "",
-            "",
-            "",),
-        TestimonialsResponse(
-            3,
-            "John Doe",
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "",
-            "",
-            "",),
-        TestimonialsResponse(
-            4,
-            "Jane Doe",
-            "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "",
-            "",
-            "",),
-    )
-
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
     }
@@ -67,7 +31,6 @@ class TestimonialsFragment : Fragment() {
     ): View {
         val testimonialsViewModel = ViewModelProvider(this)[TestimonialsViewModel::class.java]
         _binding = FragmentTestimonialsBinding.inflate(inflater, container, false)
-        initRecyclerView(testimonialsDummyData)
         return binding.root
     }
 
