514fa512b2049b5effd0f50b05206f102e28b98f
Fran <franpepper94@gmail.com>
2022-06-30 16:43:49 -0300

    Updated home design

--- app/src/main/java/com/melvin/ongandroid/viewmodel/LogInViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/LogInViewModel.kt
@@ -8,23 +8,26 @@ import androidx.lifecycle.viewModelScope
 import com.melvin.ongandroid.data.AppData
 import com.melvin.ongandroid.domain.use_case.LogInUseCase
 import com.melvin.ongandroid.model.entities.LoginRequest
-
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.util.regex.Pattern
 import javax.inject.Inject
 
 @HiltViewModel
+class LogInViewModel @Inject constructor(
     private val appData: AppData,
     private val logInUseCase: LogInUseCase,
+) : ViewModel() {
 
     private val _status = MutableLiveData<ApiStatus>()
     val status: LiveData<ApiStatus> = _status
     val logInUserCharging = MutableLiveData(false)
 
+    fun validateEmail(Email: String): Boolean {
         return Email.isNotEmpty() && PatternsCompat.EMAIL_ADDRESS.matcher(Email).matches()
     }
 
+    fun validatePassword(password: String): Boolean {
         val passwordRegex = Pattern.compile(
             "^" +
                     "(?=.*[0-9])" +         //at least 1 digit
@@ -38,6 +41,7 @@ import javax.inject.Inject
         return password.isNotEmpty() && passwordRegex.matcher(password).matches()
     }
 
+    fun logInUser(logIn: LoginRequest) {
         logInUserCharging.postValue(true)
         viewModelScope.launch {
             val apiLogIn = logInUseCase.logInUser(logIn)
@@ -45,6 +49,7 @@ import javax.inject.Inject
             if (apiLogIn.success) {
                 _status.value = ApiStatus.SUCCESS
                 appData.saveKey(apiLogIn.data.token)
+            } else {
                 _status.value = ApiStatus.FAILURE
             }
         }
