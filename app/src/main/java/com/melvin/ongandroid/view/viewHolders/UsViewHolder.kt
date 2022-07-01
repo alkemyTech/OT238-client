89024b7f5fe6a30efdda0541a9b780a19e3d42f7
Xavier Eloy Rosales <43305416+xvirs@users.noreply.github.com>
2022-07-01 18:54:25 -0300

    fragment us y navigation

---
+++ app/src/main/java/com/melvin/ongandroid/view/viewHolders/UsViewHolder.kt
@@ -0,0 +1,20 @@
+package com.melvin.ongandroid.view.viewHolders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.melvin.ongandroid.databinding.FragmentUsItemBinding
+import com.melvin.ongandroid.model.entities.we.UsResponse
+import com.squareup.picasso.Picasso
+import javax.inject.Inject
+
+class UsViewHolder
+@Inject constructor(view: View): RecyclerView.ViewHolder(view) {
+
+    val binding = FragmentUsItemBinding.bind(view)
+
+    fun drawUs(us: UsResponse) {
+        Picasso.get().load(us.image).into(binding.ivUsImageUser)
+        binding.tvUsNameTitle.text = us.name
+        binding.tvUsUserDescription.text = us.description
+    }
+}
