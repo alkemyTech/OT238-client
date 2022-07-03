8b7e101a5ff670992ed0d3321c7784e14e94b304
LuchoCassani <lucho_92@hotmail.com>
2022-07-03 17:28:24 -0300

    implement nvaController to detail members fragment

--- app/src/main/java/com/melvin/ongandroid/view/adapters/UsAdapter.kt
+++ app/src/main/java/com/melvin/ongandroid/view/adapters/UsAdapter.kt
@@ -5,15 +5,13 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.melvin.ongandroid.R
-import com.melvin.ongandroid.model.entities.we.Us
-import com.melvin.ongandroid.model.entities.we.UsResponse
+import com.melvin.ongandroid.model.entities.we.Member
 import com.melvin.ongandroid.view.fragment.UsFragmentDirections
-import com.melvin.ongandroid.view.viewHolders.TestimonialsViewHolder
 import com.melvin.ongandroid.view.viewHolders.UsViewHolder
 import javax.inject.Inject
 
 class UsAdapter @Inject constructor(
-    private val us: List<Us>
+    private val us: List<Member>
     ): RecyclerView.Adapter<UsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsViewHolder {
@@ -27,7 +25,6 @@ class UsAdapter @Inject constructor(
         val item = us[position]
         holder.drawUs(item)
 
-        
         holder.itemView.setOnClickListener { mView ->
             val direction = UsFragmentDirections
                 .actionUsFragmentToMembersFragment2(item)
