d33b179bd2287880dd2692d979a172218e59c31f
LuchoCassani <lucho_92@hotmail.com>
2022-07-03 18:08:24 -0300

    make nullable objects so they can safe travel with safe args

--- app/src/main/java/com/melvin/ongandroid/model/entities/we/Member.kt
+++ app/src/main/java/com/melvin/ongandroid/model/entities/we/Member.kt
@@ -2,20 +2,20 @@ package com.melvin.ongandroid.model.entities.we
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Member (
     val id: Int,
-    val name: String,
-    val image: String,
-    val description: String,
-    val facebookURL: String,
-    val linkedinURL: String,
+    val name: String?,
+    val image: String?,
+    val description: String?,
+    val facebookURL: String?,
+    val linkedinURL: String?,
     @SerializedName("created_at")
-    val createdAt: String,
+    val createdAt: String?,
     @SerializedName("updated_at")
-    val updatedAt: String,
+    val updatedAt: String?,
     @SerializedName("deleted_at")
-    val deletedAt: String
+    val deletedAt: String?
 ):Parcelable
