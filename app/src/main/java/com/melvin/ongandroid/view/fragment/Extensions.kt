569a7b09bc3915453a897239658505a119439942
Simón G.M <simon.garciam@udea.edu.co>
2022-07-03 21:49:37 -0500

    Añadiendo progress Bar en Extensions.kt y en FragmentUs.xml

--- app/src/main/java/com/melvin/ongandroid/view/fragment/Extensions.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/Extensions.kt
@@ -2,6 +2,7 @@ package com.melvin.ongandroid.view.fragment
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.melvin.ongandroid.databinding.FragmentHomeBinding
+import com.melvin.ongandroid.databinding.FragmentUsBinding
 
 
 fun Fragment.showProgressBar(view: View){
@@ -14,4 +15,16 @@ fun Fragment.hideProgressBar(view: View){
     var binding: FragmentHomeBinding?=null
     binding = FragmentHomeBinding.bind(view)
     binding.pbGeneralCharging.visibility=View.GONE
+}
+
+fun Fragment.showProgressBar2(view: View){
+    var binding: FragmentUsBinding?=null
+    binding = FragmentUsBinding.bind(view)
+    binding.pbChargingUs.visibility=View.VISIBLE
+}
+
+fun Fragment.hideProgressBar2(view: View){
+    var binding: FragmentUsBinding?=null
+    binding = FragmentUsBinding.bind(view)
+    binding.pbChargingUs.visibility=View.GONE
 }
