5f3d54e393b787868e4e978a03d3c4ef2f62aadb
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-07-08 17:15:57 -0300

    Created methods on existing NEWSAdapter and NEWSViewHolder

--- app/src/main/java/com/melvin/ongandroid/view/fragment/NewsFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragment/NewsFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.FragmentNewsBinding
+import com.melvin.ongandroid.model.entities.news.News
+import com.melvin.ongandroid.view.adapters.NewsAdapter
 import com.melvin.ongandroid.viewmodel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +21,7 @@ class NewsFragment: Fragment() {
     private val viewModel: NewsViewModel by viewModels()
     private var _binding: FragmentNewsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,5 +36,47 @@ class NewsFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.title =
             resources.getString(R.string.menu_news)
+        initRecyclerView(newsDummyData)
+    }
+
+    private var newsDummyData = listOf(
+        News(
+            1,
+            "Tokyo",
+            "",
+            "Konnichiwa, sushi, naruto",
+            "https://media-cdn.tripadvisor.com/media/photo-s/15/e6/9b/47/tokyo-nightlife.jpg",
+            1,
+            1,
+            "",
+            ""
+        ),
+        News(
+            2,
+            "Los Angeles",
+            "",
+            "Home of Hollywood and great beaches",
+            "https://media-cdn.tripadvisor.com/media/photo-s/15/e6/9b/47/tokyo-nightlife.jpg",
+            1,
+            1,
+            "",
+            ""
+        ),
+        News(
+            3,
+            "Mar del Plata",
+            "",
+            "Home of great churros",
+            "https://media-cdn.tripadvisor.com/media/photo-s/15/e6/9b/47/tokyo-nightlife.jpg",
+            1,
+            1,
+            "",
+            ""
+        )
+    )
+
+    private fun initRecyclerView(data: List<News>) {
+        adapter = NewsAdapter(data, false)
+        binding.rvNews.adapter = adapter
     }
 }
--- app/src/main/java/com/melvin/ongandroid/view/viewHolders/NewsViewHolder.kt
+++ app/src/main/java/com/melvin/ongandroid/view/viewHolders/NewsViewHolder.kt
@@ -2,6 +2,7 @@ package com.melvin.ongandroid.view.viewHolders
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.melvin.ongandroid.databinding.FragmentNewsItemBinding
 import com.melvin.ongandroid.databinding.NewsViewPagerItemHomeBinding
 import com.melvin.ongandroid.model.entities.news.News
 import com.squareup.picasso.Picasso
@@ -9,11 +10,18 @@ import javax.inject.Inject
 
 class NewsViewHolder @Inject constructor(view: View): RecyclerView.ViewHolder(view) {
 
-    val binding = NewsViewPagerItemHomeBinding.bind(view)
+    private val binding = NewsViewPagerItemHomeBinding.bind(view)
+    private val newsBinding = FragmentNewsItemBinding.bind(view)
 
     fun drawNewsViewPager(news: News) {
         Picasso.get().load(news.image).into(binding.ivNews)
         binding.tvNewsTitle.text = news.name
         binding.tvNewsDescription.text = news.rawContent
+    }
+
+    fun drawNewsRecyclerView(news: News) {
+        Picasso.get().load(news.image).into(newsBinding.ivNewsItem)
+        newsBinding.tvNewsItemTitle.text = news.name
+        newsBinding.tvNewsItemDescription.text = news.rawContent
     }
 }
