842a9d07234d67ad3229f517444e7c8d7851d85a
pableArg <pablo.jc95@hotmail.com>
2022-07-17 21:28:57 -0300

    Changed network

--- app/src/main/java/com/melvin/ongandroid/view/activity/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/activity/MainActivity.kt
@@ -7,17 +7,32 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import com.melvin.ongandroid.databinding.ActivityMainBinding
+import com.melvin.ongandroid.utils.CheckNetworkConnection
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var checkNetworkConnection: CheckNetworkConnection
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        drawLayout()
+    }
+
+    private fun callNetWorkConnection() {
+        checkNetworkConnection = CheckNetworkConnection(application)
+        checkNetworkConnection.observe(this) { isConnected ->
+            if (isConnected) {
+                binding.fragmentContainerView.visibility = VISIBLE
+                binding.cNotInternet.visibility = GONE
+            } else {
+                binding.fragmentContainerView.visibility = GONE
+                binding.cNotInternet.visibility = VISIBLE
+
+            }
+        }
     }
 
     private fun isNetworkAvailable(): Boolean {
@@ -26,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         return (capabilities != null && capabilities.hasCapability(NET_CAPABILITY_INTERNET))
     }
+
 
     private fun drawLayout() {
         if (isNetworkAvailable()) {
