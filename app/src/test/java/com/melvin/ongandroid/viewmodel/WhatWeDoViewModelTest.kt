a913ffb3e45147aeb89b1ae9a2fba6b784014113
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-07-18 17:24:57 -0300

    Added RunWithJUnit

--- app/src/test/java/com/melvin/ongandroid/viewmodel/WhatWeDoViewModelTest.kt
+++ app/src/test/java/com/melvin/ongandroid/viewmodel/WhatWeDoViewModelTest.kt
@@ -9,17 +9,17 @@ import com.melvin.ongandroid.model.entities.us.MembersResponse
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.RelaxedMockK
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
 @ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
 class WhatWeDoViewModelTest {
 
     @RelaxedMockK
