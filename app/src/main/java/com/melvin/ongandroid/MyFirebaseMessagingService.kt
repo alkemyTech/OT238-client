d880b08ed2c5f5c2f6dace220116847c661902fb
Fran <franpepper94@gmail.com>
2022-07-26 13:09:20 -0300

    -Lint fixes

--- app/src/main/java/com/melvin/ongandroid/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/melvin/ongandroid/MyFirebaseMessagingService.kt
@@ -1,6 +1,13 @@
 package com.melvin.ongandroid
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        super.onNewToken(token)
+    }
 }
